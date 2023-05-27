9dc7f78e14750f0801ab1711c9af3b12757640e9
Moe Bamba <djigal001@gmail.com>
2023-05-27 00:21:55 +0000

    Cloud-Master

---
+++ cloudapp/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <iframe width="520" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" id="gmap_canvas" src="https://maps.google.com/maps?width=520&amp;height=400&amp;hl=en&amp;q=Centre%20Sauvegarde%20Dakar+(Home)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
+    </div>
+  );
+}
+
+export default App;
